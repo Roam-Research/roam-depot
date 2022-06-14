5c4f04fd9407ce37d4de0e6ae2c1ecb4763a7495
Nikita Prokopov <niki@tonsky.me>
2022-06-14 21:05:44 +0200

    Publish comment from clojure script

--- src/community_extensions/comment.clj
+++ src/community_extensions/comment.clj
@@ -1,5 +1,7 @@
 (ns community-extensions.comment
   (:require
+    [cheshire.core :as json]
+    [clj-http.client :as http]
     [clojure.java.io :as io]
     [clojure.java.shell :as shell]
     [clojure.string :as str]
@@ -8,14 +10,9 @@
 (def args-map
   (apply array-map *command-line-args*))
 
-; (defn build [ext-id data]
-;   (let [dir     (io/file "checkout" ext-id)
-;         exists? (.exists dir)
-;         {repo "source_repo"
-;          commit "source_commit"} data]
-
-
-(defn -main [& {pr "--pr" :as args-map}]
+(defn -main [& {pr "--pr"
+                token "--token"
+                :as args-map}]
   (let [branch  (str "pr-" pr)
         _       (core/sh "git" "fetch" "origin" (str "pull/" pr "/head:" branch))
         changes (vec
@@ -33,25 +30,32 @@
         after   (into {}
                   (for [[mode path] changes
                         :when (.exists (io/file path))]
-                    [path (core/slurp-json path)]))]
-    (println
-      (str/join "\n"
-        (concat
-          ["Here’s your link to the diff:"]
-          (for [[mode path] changes
-                :let [[_ user repo] (re-matches #"extensions/([^/]+)/([^/]+)\.json" path)
-                      commit-before (:source_commit (before path))
-                      commit-after  (:source_commit (after path))
-                      url (or (:source_url (before path))
-                            (:source_url (after path)))]
-                :when (not= commit-before commit-after)]
-            (cond
-              (nil? commit-before)
-              (str "Added [" user "/" repo "](" url ") [" (subs commit-after 0 7) "](" url "/tree/" commit-after ")")
+                    [path (core/slurp-json path)]))
+        message (str/join "\n"
+                  (concat
+                    ["Here’s your link to the diff:\n"]
+                    (for [[mode path] changes
+                          :let [[_ user repo] (re-matches #"extensions/([^/]+)/([^/]+)\.json" path)
+                                commit-before (:source_commit (before path))
+                                commit-after  (:source_commit (after path))
+                                url (or (:source_url (before path))
+                                      (:source_url (after path)))]
+                          :when (not= commit-before commit-after)]
+                      (cond
+                        (nil? commit-before)
+                        (str "Added [" user "/" repo "](" url ") [" (subs commit-after 0 7) "](" url "/tree/" commit-after ")")
             
-              (nil? commit-after)
-              (str "Added [" user "/" repo "](" url ") [" (subs commit-before 0 7) "](" url "/tree/" commit-before ")")
+                        (nil? commit-after)
+                        (str "Added [" user "/" repo "](" url ") [" (subs commit-before 0 7) "](" url "/tree/" commit-before ")")
             
-              :else
-              (str "Changed [" user "/" repo "](" url ") [" (subs commit-before 0 7) " → " (subs commit-after 0 7) "](" url "/compare/" commit-before ".." commit-after ")"))))))
+                        :else
+                        (str "Changed [" user "/" repo "](" url ") [" (subs commit-before 0 7) " → " (subs commit-after 0 7) "](" url "/compare/" commit-before ".." commit-after ")")))))]
+    (println message)
+    (when token
+      (println
+        (http/post (str "https://api.github.com/repos/Roam-Research/roam-marketplace/issues/" pr "/comments")
+          {:headers {"Content-Type" "application/json"
+                     "Accept" "application/vnd.github.v3+json"
+                     "Authorization" (str "token " token)}
+           :body    (json/generate-string {:body message})})))
     (shutdown-agents)))
