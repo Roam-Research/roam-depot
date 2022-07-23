eb85992e744a23c94396eb6abd06623ef04a30ab
Josh <brownjoshua490@gmail.com>
2022-07-22 18:57:36 -0600

    new diff

--- src/community_extensions/prBuild.clj
+++ src/community_extensions/prBuild.clj
@@ -1,5 +1,7 @@
 (ns community-extensions.prBuild
   (:require
+   [clojure.string :as str]
+   [cheshire.core :as json]
    [clojure.java.io :as io]
    [clojure.java.shell :as shell]
    [community-extensions.core :as core]))
@@ -23,11 +25,27 @@
         (core/sh "chmod" "+x" "build.sh")
         (core/sh "./build.sh")))))
 
+(defn diff [{pr "--pr"}]
+  (let [branch (str "pr-" pr)
+        _      (core/sh "git" "fetch" "origin" (str "pull/" pr "/head:" branch))]
+    (for [[mode path] (->> (core/sh "git" "diff" "--name-status" "--merge-base" "remotes/origin/main" branch)
+                           (str/split-lines)
+                           (map #(str/split % #"\t")))
+          :when       (str/starts-with? path "extensions/")
+          :let        [[_ user repo] (re-matches #"extensions/([^/]+)/([^/]+)\.json" path)
+                       ext-id        (str user "+" repo)
+                       file          (io/file path)
+                       data          (when (.exists file)
+                                       (json/parse-string (slurp file)))]]
+      [mode ext-id data])))
+
 (defn -main [& {:as args-map}]
-  (let [dif (core/diff args-map)]
+  (prn "args" args-map)
+  (let [dif (diff args-map)]
     (prn dif)
     (doseq [[mode ext-id data] dif]
       (prn mode ext-id data)
+      #_
       (case mode
         "A" (build ext-id data)
         "M" (build ext-id data)
