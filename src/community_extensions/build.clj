998309f007499f2731436ac253515fb3edd59bf6
Josh Brown <brownjoshua490@gmail.com>
2025-01-09 14:28:21 -0500

    test

--- src/community_extensions/build.clj
+++ src/community_extensions/build.clj
@@ -35,10 +35,12 @@
           (core/sh "cp" (str subdir "/extension.js") ".")
           (when (.exists (io/file sub-dir-file "extension.css"))
             (core/sh "cp" (str subdir "/extension.css") "."))
-          (prn (io/file sub-dir-file "CHANGELOG.md"))
+          ;; (prn (io/file sub-dir-file "CHANGELOG.md"))
+          #_
           (when (.exists (io/file sub-dir-file "CHANGELOG.md"))
             (prn "THIS SHOULDN'T EXIST")
             (core/sh "cp" (str subdir "/CHANGELOG.md") "."))
+          #_
           (when (.exists (io/file sub-dir-file "README.md"))
             (core/sh "cp" (str subdir "/README.md") "."))
           (print (core/sh "ls")))))))
