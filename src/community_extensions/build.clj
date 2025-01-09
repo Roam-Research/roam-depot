124b8f10e464abe20f1e85ada797de35634089a5
Josh Brown <brownjoshua490@gmail.com>
2025-01-09 14:10:45 -0500

    try supporting subdirs

--- src/community_extensions/build.clj
+++ src/community_extensions/build.clj
@@ -11,17 +11,32 @@
   (let [dir     (io/file "checkout" ext-id)
         exists? (.exists dir)
         {repo "source_repo"
-         commit "source_commit"} data]
+         commit "source_commit"
+         subdir "source_subdir"} data]
+    (prn "SUBDIR" subdir exists?)
     (when-not exists?
       (.mkdirs dir)
       (core/sh "git" "clone" repo (str "checkout/" ext-id)))
     (shell/with-sh-dir dir
       (when exists?
         (core/sh "git" "fetch" "--all"))
-      (core/sh "git" "-c" "advice.detachedHead=false" "checkout" commit)
-      (when (.exists (io/file dir "build.sh"))
-        (core/sh "chmod" "+x" "build.sh")
-        (core/sh "./build.sh")))))
+      (core/sh "git" "-c" "advice.detachedHead=false" "checkout" commit))
+    ;; if there is a sub dir, run build inside of sub directory
+    (let [sub-dir-file (if (string? subdir)
+                         (io/file dir subdir)
+                         dir)]
+      (shell/with-sh-dir sub-dir-file
+        (when (.exists (io/file sub-dir-file "build.sh"))
+          (core/sh "chmod" "+x" "build.sh")
+          (core/sh "./build.sh"))))
+    (when (string? subdir)
+      ;; copy all required files to the root because build actions require them there
+      (shell/with-sh-dir dir
+        (core/sh "cp" (str subdir "/extension.js") ".")
+        (core/sh "cp" (str subdir "/extension.css") ".")
+        (core/sh "cp" (str subdir "/CHANGELOG.md") ".")
+        (core/sh "cp" (str subdir "/README.md") ".")
+        (print (core/sh "ls"))))))
 
 (defn -main [& {:as args-map}]
   (doseq [[mode ext-id data] (core/diff args-map)]
